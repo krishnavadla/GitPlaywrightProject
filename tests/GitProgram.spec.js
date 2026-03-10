1764595ab1534d668b4833d625d259a2ff20f5b7
krishnavadla <hellosaikrishna9@gmail.com>
2026-03-10 08:34:53 +0530

    PushMyCode

---
+++ tests/GitProgram.spec.js
@@ -0,0 +1,13 @@
+import {test,expect} from '@playwright/test'
+test("Testcase-01" , async ({page}) => {
+    console.log("This is test case-01")
+})
+test("Testcase-02" , async ({page}) => {
+    console.log("This is test case-02")
+})
+test("Testcase-03" , async ({page}) => {
+    console.log("This is test case-03")
+})
+test("Testcase-04" , async ({page}) => {
+    console.log("This is test case-04")
+})
